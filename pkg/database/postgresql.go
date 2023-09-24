1f88ad6aa1e040f7c0c004ae721447b14107d8a2
Duy Huynh <vndee.huynh@gmail.com>
2023-09-24 12:12:30 +0700

    update connection

--- pkg/database/postgresql.go
+++ pkg/database/postgresql.go
@@ -21,12 +21,38 @@ func GetCloudSQLDB() (func() error, error) {
 	if Pool != nil {
 		return nil, nil
 	}
+	// cleanup, err := pgxv4.RegisterDriver("cloudsql-postgres", cloudsqlconn.WithIAMAuthN())
+	// if err != nil {
+	// 	log.Fatalf("Error on pgxv4.RegisterDriver: %v", err)
+	// }
+	// dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=disable", os.Getenv("DB_INSTANCE_CONNECTION_NAME"), os.Getenv("DB_USER"), os.Getenv("DB_PASS"), os.Getenv("DB_NAME"))
+	mustGetenv := func(k string) string {
+		v := os.Getenv(k)
+		if v == "" {
+			log.Fatalf("Fatal Error in connect_unix.go: %s environment variable not set.", k)
+		}
+		return v
+	}
+
 	cleanup, err := pgxv4.RegisterDriver("cloudsql-postgres", cloudsqlconn.WithIAMAuthN())
 	if err != nil {
 		log.Fatalf("Error on pgxv4.RegisterDriver: %v", err)
 	}
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=disable", os.Getenv("DB_INSTANCE_CONNECTION_NAME"), os.Getenv("DB_USER"), os.Getenv("DB_PASS"), os.Getenv("DB_NAME"))
-	db, err := sql.Open("cloudsql-postgres", dsn)
+	// Note: Saving credentials in environment variables is convenient, but not
+	// secure - consider a more secure solution such as
+	// Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
+	// keep secrets safe.
+	var (
+		dbUser         = mustGetenv("DB_USER")                                    // e.g. 'my-db-user'
+		dbPwd          = mustGetenv("DB_PASS")                                    // e.g. 'my-db-password'
+		dbName         = mustGetenv("DB_NAME")                                    // e.g. 'my-database'
+		unixSocketPath = "/cloudsql/" + mustGetenv("DB_INSTANCE_CONNECTION_NAME") // e.g. '/cloudsql/project:region:instance'
+	)
+
+	dbURI := fmt.Sprintf("%s:%s@unix(%s)/%s?parseTime=true",
+		dbUser, dbPwd, unixSocketPath, dbName)
+
+	db, err := sql.Open("cloudsql-postgres", dbURI)
 	if err != nil {
 		log.Fatalf("Error on sql.Open: %v", err)
 	}
