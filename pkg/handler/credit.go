278010c6658aa53d144d5d8ba77d9e9d82dd3b64
Duy Huynh <vndee.huynh@gmail.com>
2023-09-27 13:10:35 +0700

    expire subscription plan

--- pkg/handler/credit.go
+++ pkg/handler/credit.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"time"
+
 	"github.com/gofiber/fiber/v2"
 	gofiberfirebaseauth "github.com/sacsand/gofiber-firebaseauth"
 	"github.com/vndee/lensquery-backend/pkg/database"
@@ -12,8 +14,20 @@ func GetUserRemainCredits(c *fiber.Ctx) error {
 	user := c.Locals("user").(gofiberfirebaseauth.User)
 	response := database.Pool.Where("user_id = ?", user.UserID).First(&credits)
 
+	if credits.ExpiredTimestampMs < time.Now().Unix() {
+		_ = database.Pool.Model(&model.UserCredits{}).Where("user_id = ?", user.UserID).Updates(map[string]interface{}{
+			"expired_timestamp_ms":  credits.ExpiredTimestampMs,
+			"ammount_equation_snap": 0,
+			"remain_equation_snap":  0,
+			"ammount_text_snap":     0,
+			"remain_text_snap":      0,
+		})
+
+		return c.SendStatus(fiber.StatusNotFound)
+	}
+
 	if err := database.ProcessDatabaseResponse(response); err != nil {
-		return err
+		return c.SendStatus(fiber.StatusNotFound)
 	}
 
 	return c.Status(fiber.StatusOK).JSON(credits)
